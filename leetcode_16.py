4db8b958206f92b76e52917183c15ac6c708dcb2
nadill24 <cassel_wolf@naver.com>
2020-05-28 11:37:43 +0900

    0528

--- leetcode_16.py
+++ leetcode_16.py
@@ -8,22 +8,7 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        la = []
-        dis = 0
-        maxxxx = 999999999
-        for i in range(1, len(nums)-1):
-            sol = 0
-            if nums[i] + nums[i-1] + nums[i+1] == target:
-                return num[i]
-
-            else:
-                sol = nums[i] + nums[i-1] + nums[i+1]
-                dis = abs(target-sol)
-                if dis < maxxxx:
-                    maxxxx = dis
-                else:
-                    pass
-                return nums[i]
+        # 다시 올릴게ㅎㅎㅎ
 
 
 
